4cdcf14536b4e6e30be56095dc9da95e1b769426
Catherine Tucker <ImCatherineNoel@gmail.com>
2020-07-16 19:46:58 -0700

    step 1 done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,17 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/imcatherinenoel')
+  .then((successResponse) => {
+    console.log(successResponse)
+  })
+  .catch((errorResponse)=> {
+    console.log(errorResponse)
+  });
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,6 +25,7 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -35,18 +45,18 @@ const followersArray = [];
     Using DOM methods and properties, create and return the following markup:
 
     <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
+	 <img src={image url of user} />
+	 <div class="card-info">
+	   <h3 class="name">{users name}</h3>
+	   <p class="username">{users user name}</p>
+	   <p>Location: {users location}</p>
+	   <p>Profile:
+		<a href={address to users github page}>{address to users github page}</a>
+	   </p>
+	   <p>Followers: {users followers count}</p>
+	   <p>Following: {users following count}</p>
+	   <p>Bio: {users bio}</p>
+	 </div>
     </div>
 */
 
